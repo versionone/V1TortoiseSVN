5b712dc338ded47a077668c7bdca26e929ea9ebe
Ian Buchanan <ian.buchanan@versionone.com>
2012-09-12 15:09:51 -0400

    fixed problems with NuGet resolution and skipped build

--- V1TortoiseSVN/Properties/AssemblyInfo.cs
+++ V1TortoiseSVN/Properties/AssemblyInfo.cs
@@ -1,3 +1,16 @@
 ﻿using System.Reflection;
 
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 [assembly: AssemblyTitle("VersionOne SDK TortoiseSVN Sample")]
+[assembly: AssemblyCompany("VersionOne, Inc.")]
+[assembly: AssemblyCopyright("Copyright 2012, VersionOne, Inc. Licensed under modified BSD.")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyInformationalVersion("12.2.1.3584 Summer '12")]
